82170b7ac7a11dd1a38ad97b2c3f7abe52365232
Yoyo Wang <115062425+yoyostudy@users.noreply.github.com>
2024-12-14 13:57:25 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,13 @@ setup(
     name="autored",
     version="0.0.1",
     description="Automated attack generation framework for red teaming of LLMs",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",  
     author="Zhe Wang",
+    author_email="wangzheyoyo@gmail.com", 
     packages=find_packages(),
     python_requires=">=3.7",
     install_requires=requirements,
     url="https://github.com/yoyostudy/AutoRed",
+    license="Apache 2.0", 
 )
