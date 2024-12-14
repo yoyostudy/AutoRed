a2176a9d69d6d88980e0966383a86c8c026e8d30
ZHe <wangzheyoyo@gmail.com>
2024-12-14 13:48:38 -0800

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+<h1 align="center"><img src="assets/autored-logo2.png" style="vertical-align: middle" width="50px"> <b>AutoRed</b>: Automated Attack Generation Framework for Red Teaming of Large Language Models</h1>  
+
+
+🪪 LLMs pose privacy risks by retaining sensitive information in context memory, potentially leading to unintended data exposure.
+
+🛡️ Traditional red teaming is costly and slow. 
+
+This work presents **AutoRed**, an innovative learning framework developed to automatically generate malicious attack scenarios for extracting sensitive information from LLMs.
+
+**AutoRed** consists 
+
+- **One high-level model for decision-making**:
+  - The **Stop Point Identifier** is a trained binary classifier that determines whether the current stage should proceed with an attack or an extraction task.
+
+- **Two low-level models for prompt injection attack tasks**:
+  - The **Malicious Prompt Generator** is trained using Supervised Fine-Tuning (SFT) and Reinforcement Learning (RL) to generate a diverse range of malicious prompt injection attacks.
+  - The **Sensitive Information Extractor** is a few-shot engineered GPT-3.5-turbo model designed to extract sensitive data.
+
+
+
+## Acknowledgments
+
+This work uses the [**RL4LMs**](https://github.com/allenai/RL4LMs) library developed by [**AllenAI**](https://allenai.org/); see the [license](https://github.com/yoyostudy/AutoRed/blob/main/LICENSE) for details.
+
+  
---
+++ rl4lms/data_pools/custom_text_generation_pools.py
@@ -0,0 +1,53 @@
+from rl4lms.data_pools.text_generation_pool import TextGenPool, Sample
+from datasets import load_dataset
+from tqdm import tqdm
+import os
+from pathlib import Path
+import pandas
+from collections import defaultdict
+import zipfile
+import json
+
+class PIExt(TextGenPool):
+    @classmethod
+    def prepare(cls, split: str , prefix: str, concept_end_token: str, concept_separator_token: str ) -> 'TextGenPool':
+        dataset = load_dataset("json", data_files = {
+            "train": "scripts/pi/pi_data/pi_ext_data/train.json",
+            "val":  "scripts/pi/pi_data/pi_ext_data/val.json",
+            "test": "scripts/pi/pi_data/pi_ext_data/test.json",
+        })
+        dataset_split = dataset[split]
+        samples = []
+        for ix, item in enumerate(dataset_split):
+            prompt_text = prefix + "based on the llm_output:" + "\n" + item['llm_output'] 
+            ref = item["access_code"]
+            sample = Sample(
+                id = f"{split}_{ix}",
+                prompt_or_input_text= prefix + concept_separator_token + prompt_text + concept_end_token,
+                references = [ref] 
+            )
+            samples.append(sample)
+        pool_instance = cls(samples)
+        return pool_instance
+            
+
+class PIGen(TextGenPool):
+    @classmethod
+    def prepare(cls, split: str , prefix: str, concept_end_token: str, concept_separator_token: str ) -> 'TextGenPool':
+        dataset = load_dataset("json", data_files={
+            "train": "scripts/pi/pi_data/pi_gen_data/train.json",
+            "val"  : "scripts/pi/pi_data/pi_gen_data/val.json",
+            "test": "scripts/pi/pi_data/pi_gen_data/test.json"
+        })
+        dataset_split = dataset[split]
+        samples = []
+        for ix, item in enumerate(dataset_split):
+            prompt_text = 'Based on those payloads:' + item['payload'] 
+            ref_text = item['attack']
+            sample = Sample(id=f"{split}_{ix}",
+                            prompt_or_input_text= prefix + concept_separator_token + prompt_text + concept_end_token, 
+                            references=[ref_text],
+                            )
+            samples.append(sample)
+        pool_instance = cls(samples)
+        return pool_instance
