5aa7ec1fa6a1ca056cae877f01d9b910ab1cd1d5
Yoyo Wang <115062425+yoyostudy@users.noreply.github.com>
2024-12-14 13:55:33 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("requirements.txt") as fp:
     requirements = fp.read().splitlines()
 
 setup(
-    name="rl4lms",
-    version="0.2.2",
-    description="A library for training language models (LM) using RL",
-    author="Rajkumar Ramamurthy, Prithviraj Ammanabrolu",
+    name="autored",
+    version="0.0.1",
+    description="Automated attack generation framework for red teaming of LLMs",
+    author="Zhe Wang",
     packages=find_packages(),
     python_requires=">=3.7",
     install_requires=requirements,
-    url="https://github.com/allenai/RL4LMs",
+    url="https://github.com/yoyostudy/AutoRed",
 )
